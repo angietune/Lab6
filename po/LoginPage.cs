5a576bc9b1adc58fe3c3c4c6930176a1224dd3c2
Nathalie <75065099+angietune@users.noreply.github.com>
2021-06-02 18:03:18 +0300

    Add files via upload

---
+++ po/LoginPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using OpenQA.Selenium.Interactions;
+using PageFactoryCore;
+
+namespace Lab6
+{
+    class LoginPage : AbstractPage
+    {
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "Name")]
+        public IWebElement name;
+
+        [FindsBy(How = How.Id, Using = "Password")]
+        public IWebElement password;
+
+        [FindsBy(How = How.Id, Using = "submit")]
+        public IWebElement submitBtn;
+
+        public void LoginApp(string name, string password)
+        {
+            IAction action = new Actions(driver)
+                .SendKeys(name)
+                .SendKeys(password)
+                .Click(submitBtn)
+                .Build();
+
+            action.Perform();
+        }
+
+    }
+}
