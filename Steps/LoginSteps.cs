85d27551e2e44830f4685c718c639185af1ae911
Nathalie <75065099+angietune@users.noreply.github.com>
2021-06-02 18:02:31 +0300

    Add files via upload

---
+++ Steps/LoginSteps.cs
@@ -0,0 +1,42 @@
+﻿using Lab6;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject2.Steps
+{
+    [Binding]
+    public class LoginSteps
+    {
+        [Given(@"I open ""(.*)"" url")]
+        public void GivenIOpenUrl(string p0)
+        {
+            driver.Navigate().GoToUrl("http://localhost:5000/");
+        }
+
+        [When(@"I fill the following form")]
+        public void WhenIFillTheFollowingForm(string name, string password)
+        {
+            new LoginPage(driver).SendKeys(name).password.SendKeys(password);
+        }
+
+        [When(@"click the login button")]
+        public void WhenClickTheLoginButton()
+        {
+            new LoginPage.Click(submitBtn);
+        }
+
+        [Then(@"I should be at the home page")]
+        public void ThenIShouldBeAtTheHomePage()
+        {
+            Assert.AreEqual(new LoginTest(driver).logout.Text, "Logout");
+        }
+
+        [AfterScenario]
+        public void CloseDriver()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
+
